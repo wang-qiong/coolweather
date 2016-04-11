4cc0901511f89eea0e6709e3e13de277d087780f
Jerry <576209330@qq.com>
2016-04-11 19:20:28 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/model/Province.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Province {
+	private int id;
+	private String provinceName;
+	private String provinceCode;
+
+	public int getId(){
+		return id;
+	}
+	public void setId(int id){
+		this.id=id;
+	}
+	public String getProvinceName(){
+		return provinceName;
+	}
+	public void setProvinceName(String provinceNmae){
+		this.provinceName=provinceName;
+	}
+	public String getProviceCode(){
+		return provinceCode;
+	}
+	public void setProvinceCode(String provinceCode){
+		this.provinceCode=provinceCode;
+	}
+
+}
